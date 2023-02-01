8e84624146ff418d987866315bcb29c5f814a24e
MoovendhanR <moovendhanr1011@gmail.com>
2023-02-01 21:11:22 +0530

    bcrypt added and user and controller page modified

--- server/src/controllers/Notes.controller.js
+++ server/src/controllers/Notes.controller.js
@@ -1,72 +1,64 @@
-const express = require('express');
-const uploads = require('../middlewares/upload.js');
+const express=require('express');
 const Notes = require('../models/Note.model.js');
-const router =express.Router();
 
-router.get("/",async(req, res)=>{
+const router=express.Router()
+
+
+router.get('/', async(req, res) => {
     try{
-         const notes = await Notes.find()
-         .populate("userId")
-         .lean()
-         .exec();
-         res.status(200).send({notes: notes});
+        const notes=await Notes.find().lean().exec();
+        res.status(200).send({notes: notes});
     }catch(err){
-        res.status(500).send({message: err.message});
+        res.status(500).send({message:err.message})
     }
 })
 
 
-// router.post("/",uploads.single("profilePic"),async(req, res)=>{
-//     try{
-//         console.log(req.body);
-//          const note = await Notes.create({
-//              title: req.body.title,
-//              notes: req.body.notes,
-//              category: req.body.category,
-//              profilePic:req.file.path,
-//          });
-//          res.status(200).send({note: note});
-//     }catch(err){
-//         res.status(500).send({message: err.message});
-//     }
-// })
-router.post("/",async(req, res)=>{
+router.post('/create', async(req, res) => {
     try{
-         const note = await Notes.create(req.body);
-         res.status(200).send({note: note});
+        const note=await Notes.create(req.body);
+        res.status(200).send(note);
     }catch(err){
-        res.status(500).send({message: err.message});
+        res.status(500).send({message:err.message})
+    }
+})
+
+router.patch('/edit/:id', async(req, res) => {
+    //error here
+    const id = req.params.id;
+    const note = await Notes.findOne({"_id": id});
+    const userID_in_note=note.userID;
+    const userID_making_req=req.body.userID;
+    try{
+        if(userID_making_req !== userID_in_note){
+            res.status(500).send({"msg":"you are not authoriazed"})
+        }else{
+            const notes = await Notes.findByIdAndUpdate(req.params.id, req.body, {new:true}).lean().exec();
+            res.status(200).send(notes);
+        }
+    }catch(err){
+        res.status(500).send({message:err.message})
+    }
+})
+router.delete('/delete/:id', async(req, res) => {
+    const id = req.params.id;
+    const note = await Notes.findOne({"_id": id});
+    const userID_in_note=note.userID;
+    const userID_making_req=req.body.userID;
+    try{
+        if(userID_making_req !== userID_in_note){
+            res.status(500).send({"msg":"you are not authoriazed"})
+        }else{
+            const notes = await Notes.findByIdAndDelete(req.params.id).lean().exec();
+            res.status(200).send(notes);
+        }
+    }catch(err){
+        res.status(500).send({message:err.message})
     }
 })
 
 
-
-router.get("/:id",async(req, res)=>{
-    try{
-         const note = await Notes.findById(req.params.id).lean().exec();
-         res.status(200).send({note: note});
-    }catch(err){
-        res.status(500).send({message: err.message});
-    }
-})
+module.exports=router;
 
 
-router.patch("/:id",async(req, res)=>{
-    try{
-         const note = await Notes.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec();
-         res.status(200).send({note: note});
-    }catch(err){
-        res.status(500).send({message: err.message});
-    }
-})
 
-router.delete("/:id",async(req, res)=>{
-    try{
-         const note = await Notes.findByIdAndDelete(req.params.id).lean().exec();
-        res.status(200).send({note: note});
-    }catch(err){
-        res.status(500).send({message: err.message});
-    }
-})
-
-module.exports = router;
--- server/src/controllers/User.controller.js
+++ server/src/controllers/User.controller.js
@@ -1,51 +1,83 @@
-const express = require('express');
-const User = require('../models/User.model');
-const router =express.Router();
+const express =require('express');
+const mongoose = require('mongoose');
+const jwt=require("jsonwebtoken");
+const User = require('../models/User.model.js');
+const bcrypt = require('bcrypt')
 
-router.get("/",async(req, res)=>{
+
+const router = express.Router();
+
+
+router.get("/",async(req, res) => {
+     try{
+        const users = await User.find().lean().exec();
+       res.status(200).send({users:users})
+     }catch(err){
+        res.status(500).send({message:err.message});
+     }
+})
+router.post("/register",async(req, res) => {
+    const {email, password , name, age} = req.body
     try{
-         const users = await User.find().lean().exec();
-         res.status(200).send({users: users});
+       bcrypt.hash(password, 5, async(err,secure_password) => {
+              if(err) {
+               console.error(err)
+              }else{
+                 const user=await User.create({email,password:secure_password,name,age});
+                 res.status(201).send(user)
+              }
+       })
     }catch(err){
-        res.status(500).send({message: err.message});
+       res.status(500).send({message:err.message});
+    }
+})
+router.post("/login",async(req, res) => {
+    const {email, password} = req.body
+    try{
+        const user = await User.find({email});
+        const hashed_password = user[0].password
+        if(user.length > 0){
+            bcrypt.compare(password, hashed_password, (err, result) => {
+                // result == true
+                if(result) {
+                    const token = jwt.sign({ userID: user[0]._id }, 'masai');//payload ,secret key
+                    res.status(201).send({"mes":"login successfull","token":token});
+                }else{
+                    res.status(500).send("wrong credentials")
+                }
+            });
+          }else{
+             res.status.send("wrong credential")
+          }
+    }catch(err){
+       res.status(500).send({message:err.message});
     }
 })
 
-
-router.post("/",async(req, res)=>{
-    try{
-         const user = await User.create(req.body);
-         res.status(200).send({user: user});
-    }catch(err){
-        res.status(500).send({message: err.message});
-    }
-})
-router.get("/:id",async(req, res)=>{
-    try{
-         const user = await User.findById(req.params.id).lean().exec();
-         res.status(200).send({user: user});
-    }catch(err){
-        res.status(500).send({message: err.message});
-    }
+router.get('/:id', async(req, res) => {
+   try{
+       const note=await Notes.findById(req.params.id).lean().exec();
+       res.status(200).send(note);
+   }catch(err){
+       res.status(500).send({message:err.message})
+   }
 })
 
 
-router.patch("/:id",async(req, res)=>{
-    try{
-         const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec();
-         res.status(200).send({user: user});
-    }catch(err){
-        res.status(500).send({message: err.message});
-    }
-})
+// router.get("/data",async(req, res) => {
+// var token=req.headers.authorization
+// jwt.verify(token,"masai",(err,decoded)=>{
+// if(err){
+// res.send("Invalid Token")
+// console.log(err)
+// } else {
+// res.send("Data ....")
+// }
+// })
 
-router.delete("/:id",async(req, res)=>{
-    try{
-         const user = await User.findByIdAndDelete(req.params.id).lean().exec();
-         res.status(200).send({user: user});
-    }catch(err){
-        res.status(500).send({message: err.message});
-    }
-})
+// })
 
-module.exports = router;
+
+
+
+module.exports=router;
